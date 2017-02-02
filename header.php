76e7587dc6a1d3eb96b88324748a73117fb7a660
Glally <lallyGus@gmail.com>
2017-02-02 15:44:04 -0800

    Made the title and categories absolute along with aligning the search engine to the right.

--- header.php
+++ header.php
@@ -5,14 +5,16 @@
 
 <head>
 <title><?php echo get_bloginfo('name')?></title>
-<ul class="topnav">
+<div class="top">
+<ul id="title"class="topnav">
 <center>
-<a rel="canonical" class="nounderline" href="<?php echo home_url();?>"><h1><?php echo get_bloginfo( 'name' ); ?></h1></a>
+<a  rel="canonical" class="nounderline" href="<?php echo home_url();?>"><h1><?php echo get_bloginfo( 'name' ); ?></h1></a>
 </center>
 </ul>
+
+
 <ul class="nav">
-<form action="<?php echo home_url(); ?>" method="get" id="searchform" > <input type="text" class="search" name="s" placeholder="Search.." > </form> 
-  </li>
+
 	
  <?php
  $list =  array('orderby'=> DESC,
@@ -23,25 +25,23 @@ foreach ( $categories as $category ) {
 	echo '<li> <a rel="canonical" class="nounderline" href="' . get_category_link( $category->term_id ) . '"><h3>' . $category->name . '</h3></a></li> ';
 }
 ?>
-
+ <form class= "search" action="<?php echo home_url(); ?>" method="get" id="searchform" > <input type="text" class="search" name="s" placeholder="Search.." > </form> 
 	<li class="icon"> 
     <a href="javascript:void(0);" style="font-size:15px;"class="nounderline" onclick="myFunction()">&#9776;</a>
   </li>
+ 
+  </li>
   </ul>
+
 <center><h1><?php if(!is_category()&& !is_single()&& !is_search()){ bloginfo('description');} ?></center></h1>
 <center><h1><?php if(is_category()|| is_single()){
 	 $catTitle= get_the_category()[0]->cat_name;
 	echo $catTitle;} ?></center></h1>
 <center><h1><?php if(is_search()){
 			echo "results";} ?> </center></h1>
-<divclass= "NW" style="padding-left:16px">
-<script>
-function myFunction() {
-    document.getElementsByClassName("nav")[0].classList.toggle("responsive");
-}
-</script>
 
-</div>
+
+
  </center>
 <?php wp_head()?>
 </head>
