7cd7c547ef8bd9b3ea898ad31fa6f25201b64540
Glally <lallyGus@gmail.com>
2017-02-03 17:45:10 -0800

    index.php and search.php both use a function since it is the same code

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
 
 <?php get_header();include 'function.php';?>
+
 <!--<div id="main"> -->
 <!--<div id="content"> -->
 <?php $posts=query_posts($query_string."&orderby=date&order=DESC"); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<div class="post" style="display: inline-block;">
-<h1><a href="<?php the_permalink() ?>" rel="bookmark"  class="nounderline"title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1> 
-<h4>Posted on <?php the_time('F jS, Y') ?></h4> 
-
-
-<?php if(isImage()){?>
- <?php echo get_image(300,300) ?>
-<?php } ?>
-<?php if(isVideo()){?>
-			<?php echo get_video(300,300) ?>
-<?php } ?>
-
-<a class="nounderline" href="<?php echo get_permalink(); ?>"><p><class="nounderline"<?php the_excerpt();?> Read More...</p></a>
-
-</div> 
+<?php post_results()?>
 <!--<hr> --> <?php endwhile;?> <?php else: ?>
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
 
