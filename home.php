8c42803df4aad19856ecdf22fe4edcede2d5182b
Glally <lallyGus@gmail.com>
2017-02-22 16:02:08 -0800

    More latest news side added

--- home.php
+++ home.php
@@ -1,37 +1,87 @@
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<?php/* Newspaper theme 
+
+	created by Gus Lally*/ ?>
 <?php
-	get_header(); include "function.php";
+	get_header();?>
 	
+	
+
+	
+<?php	
+// List of parrent categories variable
     $list =  array('orderby'=> DESC,
   'parent' => 0
-  );
+  );?>
+
+<!-- other posts -->
+<?php $posts=query_posts($query_string."&orderby=date&order=DESC&offset=1"); ?>
+<?php if (have_posts()) : ?>			
+			<ul style="float:left;">
+            <div style="float:left;">
+			 <h3> <?php echo "more news" ; ?></h3>
+			 
+	<?php while (have_posts()) : the_post(); ?>
+
+<?php if(isImage()){?>
+		<?php echo get_image(100,100) ?>
+		<?php } ?>
+		<?php if(isVideo()){?>
+			
+			<?php echo get_video(150,100,'preview') ?>
+	<?php }?>
+			<p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
+			<hr>
+ <?php endwhile;?></div></ul><?php endif; ?>
+  
+ 
+  
+<?php $page_count =1; //page count variable
+
+// Make a list of parrent categories 1 post per category
 $categories = get_categories( $list );
     foreach ( $categories as $category ) {
 		$args = array(
     'cat' => $category->term_id,
     'post_type' => 'post',
-    'posts_per_page' => '1',
+    'posts_per_page' => $page_count,
 );
-		$query = new WP_Query( $args );
+		$query = new WP_Query( $args );?>
+	
+<center>
 
-if ( $query->have_posts() ) { ?>
+
+
+<?php if ( $query->have_posts() ) { ?>
 		
-	<li class = "noBullets">
+	
 	<h2><?php	echo ' <a rel="canonical" class="nounderline" href="' . get_category_link( $category->term_id ) . '">' .'Latest in '. $category->name . '</a> '; ?>:</h2>
      
  
         <?php while ( $query->have_posts() ) {
 				
- 
+			
             $query->the_post();
             ?>
-
-           <?php post_results();?>
- 
-        <?php } // end while ?></li>
+			
+			<div class="post" >
+           <?php if(isImage()){?>
+	<?php echo get_image() ?>
+	<?php } ?>
+	<?php if(isVideo()){?>
+			
+			<?php echo get_video(800,450,'embed') ?><br>
+	<?php }?>
+	<a class="nounderline" href="<?php echo get_permalink(); ?>"><class="nounderline"<?php   the_excerpt();?> Read More...</a>
+			</div>
+        <?php }  ?>
 		
-		
+		</center>
  
 <?php } // end if
-    }
+    }?>
 
-?>
+	
+
+<?php get_footer(); ?>
+
