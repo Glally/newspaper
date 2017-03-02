561088bde157886d80788b12956c37728e1c2cd8
Glally <lallyGus@gmail.com>
2017-03-01 21:39:51 -0800

    name of category on more works properly

--- index.php
+++ index.php
@@ -3,84 +3,11 @@
 	created by Gus Lally*/ ?>
 
 
+
+	
 <?php get_header();?>
 
-<?php
-$catTitle = $catTitle= single_cat_title('',false);
-
-// Category title_
- $catid = $category[0]->cat_ID;
- 
- // get the top level cat id of a single post
-
-$category = get_the_category($post->ID);
- 
-$catid = $category[0]->cat_ID;
-$catId = get_cat_ID($catTitle);
-
-$top_level_cat = smart_category_top_parent_id ($catid);
-if(category_has_parent($catId)){
-	echo '<ul class = "nav">'; 
-	
-	 ?> 
-
-	<div style="padding-left:16px">
-	<script>
-	function myFunction2() {
-		document.getElementsByClassName("nav")[1].classList.toggle("responsive");
-	}
-	</script>
-	</div>
-		<li class="icon"> 
-	
-    <a href="javascript:void(0);" style="font-size:15px;"class="nounderline" onclick="myFunction2()"><?php echo$catTitle;?></a>
-    </li>
-	<?php
-
-	$get_category = wp_list_categories('orderby=id&depth=1
-		&title_li=&use_desc_for_title=1&child_of='.$top_level_cat);?>
-
-</ul>
-<br>
-
-<?php }?>
-
-
-<?php
-
-$get_category = null;
- 
-	?>
-
-    
-			
-<?php if($catTitle != get_bloginfo('description') && is_category() && !empty(get_categories('orderby=id&depth=1
-    &title_li=&use_desc_for_title=1&child_of='.$catId))) {
-	echo '<ul class = "nav">';
-	//$catId-=1;
-	//echo $get_category->cat_ID;
-	//echo $catId;
-    ?><div style="padding-left:16px">
-	<script>
-	function myFunction2() {
-		document.getElementsByClassName("nav")[1].classList.toggle("responsive");
-	}
-	</script>
-	</div>
-		<li class="icon"> 
-	
-    <a href="javascript:void(0);" style="font-size:15px;"class="nounderline" onclick="myFunction2()"><?php echo$catTitle;?></a>
-    </li><?php
-    $get_category = wp_list_categories('orderby=id&depth=1
-    &title_li=&use_desc_for_title=1&child_of='.$catId);
- 
-		
-	
-		echo "</ul>"?>
- <?php }else{
-	  echo "<ul></ul>";
- } ?>
-<center><h1> <?php echo single_cat_title('') ; ?></center></h1>
+<center><h1> <?php echo single_cat_title('') ; ?></center></h1> 
 <!-- main post -->
 <ul>
 <center>
@@ -126,7 +53,7 @@ $get_category = null;
 <?php if (have_posts()) : ?>			
 			<ul style="float:to-right;">
             <div style="float:top-right;">
-			 <h3> <?php echo "more $catTitle" ; ?></h3>
+			 <h3> <?php echo "more ".single_cat_title('',false); ?></h3>
 			 
 	<?php while (have_posts()) : the_post(); ?>
 
