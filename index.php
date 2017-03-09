fc9b5d282df54457570f95cb68c08be5a2ac5761
Glally <lallyGus@gmail.com>
2017-03-08 22:30:57 -0800

    alignment issue fixed

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 
 <!-- other posts -->
 <?php $posts=query_posts($query_string."&orderby=date&order=DESC&offset=7"); ?>
-<?php if (have_posts()) : ?>			
-			<ul class = "related" style="float:left;" >
+<?php if (have_posts()) : ?>
+			<ul class = "related" style="float:left:16px;" >
             <div class="post-navigation" style="float:left;">
 			 <h3> <?php echo "more ".single_cat_title('',false); ?></h3>
 			 
@@ -26,19 +26,20 @@
 	<?php }?>
 			<p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
 			<hr>
- <?php endwhile;?></div><?php endif; ?></ul>
+ <?php endwhile;?></div></ul><?php endif; ?>
 
 
 
 <!-- main posts -->
 
 
-<center>
+
 
 <?php $posts=query_posts($query_string."&orderby=date&order=DESC&posts_per_page=7"); ?>
 <?php if (have_posts()) :?> <?php while (have_posts()) : the_post(); ?>
 
-	<div>
+	<center>
+			<div class ="post"   style= "width:38%; float:center;" >
 	
 	<h1><a href="<?php the_permalink() ?>" rel="bookmark"  class="nounderline"title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1> 
 	<h4><a class="nounderline" href="<?php the_permalink() ?>">Posted on <?php the_time('F jS, Y') ?></h4>
@@ -49,7 +50,7 @@
 	<?php } ?>
 	<?php if(isVideo()){?>
 			
-			<?php echo get_video(800,450,'embed') ?><br>
+			<center><?php echo get_video(800,450,'embed') ?></center><br>
 	<?php }?>
 	<a class="nounderline" href="<?php echo get_permalink(); ?>"><class="nounderline"<?php   the_excerpt();?> Read More...</a>
 
