4cc4d0c9d0dcd92874b10bea4a3df28f31110268
Glally <lallyGus@gmail.com>
2017-03-12 03:25:02 -0700

    Related now floats right and at bottom on small screens

--- single.php
+++ single.php
@@ -11,15 +11,43 @@
 // Get the category name
 
 get_header();?>
-<br><br><br><br><br>
+
+
+<center>
+
+	<?php $posts=query_posts($query_string."&orderby=date&order=DESC"); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	
+    <div class ="post"   style= "width:38%;float:left;" >
+       <center> <h2><?php the_title();?></h2></center>
+		
+		
+
+	<?php  add_filter( 'the_content', 'get_video_link' ); the_content();?>
+            
+
+<p  class="nounderline"><?php the_tags('Tags: ', ', ', '<br />'); ?><center><b> Posted by:</b> <?php the_author(); ?> <b>on:</b> <?php the_time('M.d, Y') ?></center> </p>
+
+
+<div class= "comment">	<?php comments_template();?> <style="display: inline-block;">	<div>
+
+
+
+</div>
+
+
+
+	
+    <?php endwhile; endif; ?>
+	</center>
 <!-- related posts -->
  <?php $related = ci_get_related_posts( get_the_ID(), -1 );
  
           if( $related->have_posts() ):
           ?>
 			
-			<ul class = "related" style="float:left;" >
-            <div class="post-navigation" style="float:left;">
+			
+            <div class="RP" >
               <h3>Related posts</h3>
               
                 <?php while( $related->have_posts() ): $related->the_post(); ?>
@@ -33,34 +61,12 @@ get_header();?>
 			<p><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
 			<hr>
                 <?php endwhile; ?>
-              </ul>
+            </div></ul>
            
           <?php
-          endif; ?>
+          endif; ?> 
 
-	<center>
-	<?php $posts=query_posts($query_string."&orderby=date&order=DESC"); ?>
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class ="post"   style= "width:38%; float:center;" >
-        <h2><?php the_title();?></h2>
-		
-		
-
-	<?php  add_filter( 'the_content', 'get_video_link' ); the_content();?>
-            
-
-<p  class="nounderline"><?php the_tags('Tags: ', ', ', '<br />'); ?><center><b> Posted by:</b> <?php the_author(); ?> <b>on:</b> <?php the_time('M.d, Y') ?></center> </p>
-
-<div class= "comment">	<?php comments_template();?> <style="display: inline-block;">	<div>
-
-</div>
-
-
-
-	
-    <?php endwhile; endif; ?>
- <br> 
  
-</center>
+
 
 <?php get_footer() ?>
